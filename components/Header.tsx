d28bb80fe0f75754ddc109b59f362b9f00858fa7
jafb321 <jafb321@gmail.com>
2022-07-02 11:02:21 -0700

    feat: Header component

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+
+export const Header = () => {
+  return (
+    <div className="w-full py-4 bg-[#0b0b0b] text-white">
+        <h1 className="text-4xl text-center">
+            Automatas Live
+        </h1>
+        
+    </div>
+  )
+}
